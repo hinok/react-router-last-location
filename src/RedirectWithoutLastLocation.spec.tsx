3706481cc762ffa860940c5dbd523d1280e79f57
JacobMGEvans <jacobmgevans@gmail.com>
2021-06-28 00:06:29 -0500

    No longer using Enzyme

--- src/RedirectWithoutLastLocation.spec.tsx
+++ src/RedirectWithoutLastLocation.spec.tsx
@@ -6,88 +6,89 @@ import { LastLocationType } from './LastLocationContext';
 import useLastLocation from './useLastLocation';
 import RedirectWithoutLastLocation from './RedirectWithoutLastLocation';
 import LastLocationProvider, { setLastLocation } from './LastLocationProvider';
+import {  screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 
-test('should ', () => {
+function rtlRender({ redirectTo }: { redirectTo: RedirectProps['to'] }) {
+  const Page: React.FC = ({ children }) => {
+    const lastLocation: LastLocationType = useLastLocation();
 
+    return (
+      <>
+        <h1>{children}</h1>
+        <nav data-testid="last-location-pathname">{lastLocation && lastLocation.pathname}</nav>
+      </>
+    );
+  };
+
+  const Home = () => <Page>Home</Page>;
+  const About = () => <Page>About</Page>;
+  const Secret: React.FC = () => <RedirectWithoutLastLocation to={redirectTo} />;
+
+  return render(
+        <MemoryRouter initialEntries={['/']}>
+          <LastLocationProvider>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/secret" component={Secret} />
+            </Switch>
+          </LastLocationProvider>
+          <Link to="/">HomeLink</Link>
+          <Link to="/about">AboutLink</Link>
+          <Link to="/secret">SecretLink</Link>
+        </MemoryRouter>
+      )
+
+}
+
+describe('RedirectWithoutLastLocation', () => {
+  const runTest = () => {
+    // Check if lastLocation is empty
+    screen.getAllByTestId(/last-location-pathname/i)
+    // Check if we are on the homepage
+    screen.getAllByRole('heading', {
+      name: /home/i
+    })
+
+    // Visit /about
+    userEvent.click(screen.getAllByText(/AboutLink/i)[0])
+     
+    // Check if we are on the /about
+    screen.getAllByRole('heading', {
+      name: /about/i
+    })
+
+    // Check lastLocation
+    let pathName = screen.getAllByTestId(/last-location-pathname/i)
+    expect(pathName[pathName.length -1]).toHaveTextContent('/')
+
+    // Visit /secret
+    userEvent.click(screen.getAllByText(/SecretLink/i)[0])
+    // We Should be redirected back to homepage
+    screen.getAllByRole('heading', {
+      name: /home/i
+    })
+
+    // The lastLocation should point to /about
+    pathName = screen.getAllByTestId(/last-location-pathname/i)
+    expect(pathName[pathName.length -1]).toHaveTextContent('/about')
+  };
+
+  describe('When `to` is passed as a string', () => {
+    it('should NOT store redirected route', () => {
+     rtlRender({ redirectTo: '/' })
+      runTest();
+      
+    });
+  });
+  describe('When `to` is passed as an object', () => {
+    rtlRender({
+      redirectTo: {
+        pathname: '/',
+      },
+    })
+    runTest();
+  });
 });
-
-// const prepareTest = ({ redirectTo }: { redirectTo: RedirectProps['to'] }) => {
-//   const Page: React.FC = ({ children }) => {
-//     const lastLocation: LastLocationType = useLastLocation();
-
-//     return (
-//       <>
-//         <div className="page">{children}</div>
-//         <div className="last-location-pathname">{lastLocation && lastLocation.pathname}</div>
-//       </>
-//     );
-//   };
-
-//   const Home = () => <Page>Home</Page>;
-//   const About = () => <Page>About</Page>;
-//   const Secret: React.FC = () => <RedirectWithoutLastLocation to={redirectTo} />;
-
-//   const App: React.FC = () => (
-//     <MemoryRouter initialEntries={['/']}>
-//       <LastLocationProvider>
-//         <Switch>
-//           <Route exact path="/" component={Home} />
-//           <Route exact path="/about" component={About} />
-//           <Route exact path="/secret" component={Secret} />
-//         </Switch>
-//       </LastLocationProvider>
-//       <Link to="/">Home</Link>
-//       <Link to="/about">About</Link>
-//       <Link to="/secret">Secret</Link>
-//     </MemoryRouter>
-//   );
-
-//   setLastLocation(null);
-//   const wrapper = mount(<App />);
-//   return { wrapper };
-// };
-
-// describe('RedirectWithoutLastLocation', () => {
-//   const runTest = (wrapper: ReactWrapper) => {
-//     // Check if lastLocation is empty
-//     expect(wrapper.find('.last-location-pathname').text()).toBe('');
-
-//     // Check if we are on the homepage
-//     expect(wrapper.find('.page').text()).toBe('Home');
-
-//     // Visit /about
-//     wrapper.find('a[href="/about"]').simulate('click', { button: 0 });
-
-//     // Check if we are on the /about
-//     expect(wrapper.find('.page').text()).toBe('About');
-
-//     // Check lastLocation
-//     expect(wrapper.find('.last-location-pathname').text()).toBe('/');
-
-//     // Visit /secret
-//     wrapper.find('a[href="/secret"]').simulate('click', { button: 0 });
-
-//     // We Should be redirected back to homepage
-//     expect(wrapper.find('.page').text()).toBe('Home');
-
-//     // The lastLocation should point to /about
-//     expect(wrapper.find('.last-location-pathname').text()).toBe('/about');
-//   };
-
-//   describe('When `to` is passed as a string', () => {
-//     it('should NOT store redirected route', () => {
-//       const { wrapper } = prepareTest({ redirectTo: '/' });
-//       runTest(wrapper);
-//     });
-//   });
-
-//   describe('When `to` is passed as an object', () => {
-//     const { wrapper } = prepareTest({
-//       redirectTo: {
-//         pathname: '/',
-//       },
-//     });
-//     runTest(wrapper);
-//   });
-// });
